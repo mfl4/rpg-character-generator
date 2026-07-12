e80885f4af9a5f00a655dce003187baa415f5c8a
mfl4 <91680736+mfl4@users.noreply.github.com>
2026-07-12 23:17:35 +0700

    refactor: add stat distribution rules and total points validation

--- main.py
+++ main.py
@@ -11,3 +11,14 @@ def create_character(name, strength, intelligence, charisma):
         return "The character name is too long"
     if " " in name:
         return "The character name should not contain spaces"
+
+    if type(strength) is not int or type(intelligence) is not int or type(charisma) is not int:
+        return "All stats should be integers"
+
+    if strength < 1 or intelligence < 1 or charisma < 1:
+        return "All stats should be no less than 1"
+    if strength > 4 or intelligence > 4 or charisma > 4:
+        return "All stats should be no more than 4"
+
+    if strength + intelligence + charisma != 7:
+        return "The character should start with 7 points"
